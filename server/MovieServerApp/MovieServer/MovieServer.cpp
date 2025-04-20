995ae8c2ffa0a78c339a6051684093a7e53344e6
GabrielmAlves <gabrielmalves10@gmail.com>
2025-04-20 01:23:08 -0300

    crow delete and httplib test to run server

--- server/MovieServerApp/MovieServer/MovieServer.cpp
+++ server/MovieServerApp/MovieServer/MovieServer.cpp
@@ -1,12 +1,15 @@
-﻿// MovieServer.cpp: define o ponto de entrada para o aplicativo.
-//
-
-#include "MovieServer.h"
-
-using namespace std;
+﻿#include "httplib.h"
+#include <iostream>
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	httplib::Server server;
+
+	server.Post("/recomendar", [](const httplib::Request& req, httplib::Response& res) {
+		std::string input = req.body;
+		std::cout << "Recebido: " << input << std::endl;
+	});
+	std::cout << "Servidor rodando em http://localhost:5000" << std::endl;
+	server.listen("0.0.0.0", 5000);
 	return 0;
 }
