9745cab62fbe01d0fe0a836efe3e507085757373
GabrielmAlves <gabrielmalves10@gmail.com>
2025-05-19 06:32:41 -0300

    initialize utils class, still needs porting to cpp file

--- server/MovieServerApp/MovieServer/include/utils.h
+++ server/MovieServerApp/MovieServer/include/utils.h
@@ -1,8 +1,22 @@
+#define CPPHTTPLIB_OPENSSL_SUPPORT
 #include "../httplib.h"
+#include "../../include/json.hpp"
+#include <iostream>
 
 class utils {
 public:
 	std::string GetToken(httplib::SSLClient sslClient) {
 		auto tokenResponse = sslClient.Get("/token");
+		std::string static token;
+
+		if (tokenResponse && tokenResponse->status == 200) {
+			using json = nlohmann::json;
+			auto tokenBody = json::parse(tokenResponse->body);
+			token = tokenBody["token"];
+
+			std::cout << "Token obtido: " << token << std::endl;
+		} else {
+			std::cout << "Erro ao obter o token!" << std::endl;
+		}
 	}
 };
